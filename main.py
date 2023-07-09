f965988762089f22322ff31d95e9c7c86cf6fa7d
Jia Kai Chang <changjiakai@gmail.com>
2023-07-10 02:48:19 +0800

    working now

--- main.py
+++ main.py
@@ -1,30 +1,53 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import TimeoutException
+import undetected_chromedriver as uc
 
 import time
 from datetime import datetime
 import pyautogui
 
+# *** EDIT LOGIN DETAILS ****
+# don't worry, only you can see this
+email = "insert_your_email"
+password = "insert_your_password"
+
 options = Options()
-options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(service=Service(
-    ChromeDriverManager().install()), options=options)
+# bug to be fixed: detach option not regonized
+# options.add_experimental_option("detach", True)
+driver = uc.Chrome(options=options)
+
+
+def spin_wheel():
+    while True:
+        try:
+            WebDriverWait(driver, 5).until(EC.presence_of_element_located(
+                (By.XPATH, "/html/body/div/div/div/div[3]/div/div[2]/div/div/div[6]")))
+            driver.find_element(
+                By.XPATH, "/html/body/div/div/div/div[3]/div/div[2]").click()
+            WebDriverWait(driver, 30).until(EC.element_to_be_clickable(
+                (By.XPATH, "/html/body/div/div/div/div[3]/div/div[3]/div"))).click()
+            print(datetime.now().time().strftime(
+                "[%H:%M:%S]"), "Spun daily wheel")
+            break
+        except TimeoutException:
+            print(datetime.now().time().strftime(
+                "[%H:%M:%S]"), "Daily wheel unavailable")
+            break
+
 
 # log in to blooket
 driver.get("https://id.blooket.com/login")
 driver.maximize_window()
-WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
+wait = WebDriverWait(driver, 15)
+wait.until(EC.element_to_be_clickable(
     (By.XPATH, "/html/body/div[1]/div/div/div[2]/form/div[4]/input")))
 driver.find_element(
-    By.XPATH, "/html/body/div[1]/div/div/div[2]/form/div[4]/input").send_keys("email")
+    By.XPATH, "/html/body/div[1]/div/div/div[2]/form/div[4]/input").send_keys(email)
 driver.find_element(
-    By.XPATH, "/html/body/div[1]/div/div/div[2]/form/div[5]/input").send_keys("password")
+    By.XPATH, "/html/body/div[1]/div/div/div[2]/form/div[5]/input").send_keys(password)
 driver.find_element(
     By.XPATH, "/html/body/div[1]/div/div/div[2]/form/input").click()
 time.sleep(4)
@@ -33,23 +56,23 @@ print(datetime.now().time().strftime(
 
 # my sets then host
 driver.find_element(By.XPATH, "/html/body/div/div/div/div[1]/a[7]").click()
-WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
+wait.until(EC.element_to_be_clickable(
     (By.XPATH, "/html/body/div/div/div/div[7]/div[3]/div/div[10]/div[3]"))).click()
 time.sleep(4)
 
 # in host tab, select game mode and host game
 hostURl = driver.window_handles[1]
 driver.switch_to.window(hostURl)
-WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
+wait.until(EC.element_to_be_clickable(
     (By.XPATH, "/html/body/div/div/div/div[3]/div[2]/div/div[9]"))).click()
-WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
+wait.until(EC.element_to_be_clickable(
     (By.XPATH, "/html/body/div/div/div/div[3]/div[3]/div[2]/div[2]"))).click()
 
 # set time and host now
-time_input = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
+time_input = wait.until(EC.element_to_be_clickable(
     (By.XPATH, "/html/body/div/div/div/div[3]/div[2]/div[4]/input")))
 driver.execute_script("arguments[0].value = ''", time_input)
-time_input.send_keys('6')
+time_input.send_keys('1')
 time.sleep(1)
 driver.find_element(
     By.XPATH, "/html/body/div/div/div/div[3]/div[2]/div[2]").click()
@@ -57,7 +80,7 @@ print(datetime.now().time().strftime(
     "[%H:%M:%S]"), "Cafe game hosted")
 
 # copy game code
-code = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+code = wait.until(EC.presence_of_element_located(
     (By.ID, "idNum"))).text
 
 # in game tab, enter cafe game
@@ -65,14 +88,14 @@ gameURL = driver.window_handles[0]
 driver.switch_to.window(gameURL)
 driver.get("https://play.blooket.com/play")
 
-# submit game code
-WebDriverWait(driver, 2).until(EC.element_to_be_clickable(
+# enter game code
+wait.until(EC.element_to_be_clickable(
     (By.XPATH, "/html/body/div/div/div/div[2]/div/form/div[2]/div[1]/input"))).send_keys(code)
 driver.find_element(
     By.XPATH, "/html/body/div/div/div/div[2]/div/form/div[2]/div[2]").click()
 time.sleep(4)
 
-# choose name
+# enter name
 driver.find_element(
     By.XPATH, "/html/body/div/div/div/div[2]/div/form/div[2]/div[1]/input").send_keys("KaiBot")
 driver.find_element(
@@ -91,9 +114,9 @@ print(datetime.now().time().strftime(
 
 # in game tab, clear popups
 driver.switch_to.window(gameURL)
-driver.find_element(
-    By.XPATH, "/html/body/div/div/div/div[3]/form/div[2]/div/div[2]/div[3]").click()
-WebDriverWait(driver, 5).until(EC.element_to_be_clickable(
+wait.until(EC.element_to_be_clickable(
+    (By.XPATH, "/html/body/div/div/div/div[3]/form/div[2]/div/div[2]/div[3]"))).click()
+wait.until(EC.element_to_be_clickable(
     (By.XPATH, "/html/body/div/div/div/div[3]/form/div[2]/div/div"))).click()
 time.sleep(1)
 
@@ -110,11 +133,33 @@ y = element_location["y"] + panel_height + 44  # chrome controlled label height
 # start autoclick
 pyautogui.PAUSE = 0.00001
 print(datetime.now().time().strftime(
-    "[%H:%M:%S]"), "Autoclicking now for 6 minutes...")
-end_time = time.time() + 6 * 60
+    "[%H:%M:%S]"), "Autoclicking now for 6 minutes")
+end_time = time.time() + 60
 while True:
     pyautogui.click(x, y)
     if time.time() > end_time:
         break
+
+while True:
+    try:
+        time.sleep(8)
+        WebDriverWait(driver, 5).until(EC.presence_of_element_located(
+            (By.XPATH, "/html/body/div/div/div/div[3]/div")))
+        # spin wheel if available
+        spin_wheel()
+        # click thanks
+        WebDriverWait(driver, 30).until(EC.element_to_be_clickable(
+            (By.XPATH, "/html/body/div/div/div/div[3]/div/div[2]/div[2]/div"))).click()
+        print(datetime.now().time().strftime(
+            "[%H:%M:%S]"), "Rewards collected!")
+        break
+    except TimeoutException:
+        # daily limit reached
+        print(datetime.now().time().strftime(
+            "[%H:%M:%S]"), "Daily limit reached, no coins rewarded")
+        break
+
 print(datetime.now().time().strftime(
     "[%H:%M:%S]"), "Done. Thankyou for using KaiBot!")
+
+x = input("hi:")
